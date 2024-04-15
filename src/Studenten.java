85020511ddfe2424e5af821e0425740440410c6a
Maria <166034674+mardoe10@users.noreply.github.com>
2024-04-15 13:36:29 +0200

    Klasse Studenten Aufgabe 2

--- src/Studenten.java
+++ src/Studenten.java
@@ -1,3 +1,19 @@
 public class Studenten {
 
+    private String name;
+    private int matrikelnummer;
+    private String studiengang;
+
+    // Konstruktor
+    public Studenten(String name, int matrikelnummer, String studiengang) {
+        this.name = name;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+
+    // Öffentliche Methode zum Fragenstellen
+    public void frageStellen(String frage) {
+        System.out.println(name + " :"+ frage);
+    }
 }
+
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        // Erzeugen eines Studenten-Objekts
+        Studenten student1 = new Studenten("Maria Dörfler", 123456, "Informatik");
+        // Aufruf der Methode "frageStellen"
+        student1.frageStellen("Was kommt alles in der Prüfung dran?");
+    }
+}
