d307002440dd7b54af93c08a915c597e5fbcf6d3
krboja <krb9461@thi.de>
2024-04-17 08:17:51 +0100

    Aufgabe 2 Dozierender

---
+++ src/Dozierender.java
@@ -0,0 +1,25 @@
+public class Dozierender {
+    // Datenfelder
+    private String name;
+    private String fakultaet;
+    private String bueronummer;
+
+    // Konstruktor
+    public Dozierender(String name, String fakultaet, String bueronummer) {
+        this.name = name;
+        this.fakultaet = fakultaet;
+        this.bueronummer = bueronummer;
+    }
+
+    // Öffentliche Methode zum Beantworten von Fragen
+    public void frageBeantworten(Studenten studenten, String antwort) {
+        System.out.println(name + " antwortet " + studenten.getName() + ": " + antwort);
+    }
+
+    // Getter für den Namen
+    public String getName() {
+        return name;
+    
+    }
+
+}
